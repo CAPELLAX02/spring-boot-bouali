e6d7da28436059c11ef4e25332b507a4e5e67464
CAPELLAX02 <ahmtatar@gmail.com>
2024-09-23 17:25:18 +0300

    Spring Data JPA session starter

--- example/src/main/java/com/capellax/example/FirstController.java
+++ example/src/main/java/com/capellax/example/FirstController.java
@@ -17,33 +17,4 @@ public class FirstController {
         return "Request accepted and message is: " + message;
     }
 
-    @PostMapping("/order")
-    public String order(
-            @RequestBody Order order
-    ) {
-        return "Order reveived: " + order.toString();
-    }
-
-    @PostMapping("/post-order-record")
-    public String orderRecord(
-            @RequestBody OrderRecord order
-    ) {
-        return "Order reveived: " + order.toString();
-    }
-
-    @GetMapping("/hello/{user-name}")
-    public String pathVar(
-            @PathVariable("user-name") String userName
-    ) {
-        return "my value = " + userName;
-    }
-
-    @GetMapping("/hello")
-    public String paramVar(
-            @RequestParam("user-name") String userName,
-            @RequestParam("user-lastname") String userLastname
-    ) {
-        return "my value = " + userName + " " + userLastname;
-    }
-
 }
