2acafbe05c5dc61e72d583cd5a8ce542740b38b9
CAPELLAX02 <ahmtatar@gmail.com>
2024-09-29 18:31:41 +0300

    StudentMapper Tests completed using JUnit.

--- example/src/test/java/com/capellax/example/student/StudentMapperTest.java
+++ example/src/test/java/com/capellax/example/student/StudentMapperTest.java
@@ -1,7 +1,57 @@
 package com.capellax.example.student;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class StudentMapperTest {
 
+    private StudentMapper studentMapper;
 
+    @BeforeEach
+    void setUp() {
+        studentMapper = new StudentMapper();
+    }
+
+    @Test
+    void shouldMapStudentDtoToStudent() {
+        StudentDTO studentDTO = new StudentDTO(
+                "John",
+                "Doe",
+                "john@email.com",
+                1
+        );
+
+        Student student = studentMapper.toStudent(studentDTO);
+
+        assertEquals(studentDTO.firstName(), student.getFirstName());
+        assertEquals(studentDTO.lastName(), student.getLastName());
+        assertEquals(studentDTO.email(), student.getEmail());
+        assertNotNull(student.getSchool());
+        assertEquals(studentDTO.schoolId(), student.getSchool().getId());
+
+    }
+
+    @Test
+    void shouldMapStudentToStudentResponseDto() {
+        // Given
+        Student student = new Student(
+                "Jane",
+                "Smith",
+                "jane@email.com",
+                24
+        );
+
+        // When
+        StudentResponseDTO studentResponseDTO = studentMapper.toStudentResponseDTO(student);
+
+        // Then
+        assertEquals(studentResponseDTO.firstName(), student.getFirstName());
+        assertEquals(studentResponseDTO.lastName(), student.getLastName());
+        assertEquals(studentResponseDTO.email(), student.getEmail());
+
+    }
 
 }
