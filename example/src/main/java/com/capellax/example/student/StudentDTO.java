df7fa73adad5dc5380548038da8ec1c16506a12a
CAPELLAX02 <ahmtatar@gmail.com>
2024-09-29 16:32:00 +0300

    Custom validation messages & application.yml: typo error fixed.

--- example/src/main/java/com/capellax/example/student/StudentDTO.java
+++ example/src/main/java/com/capellax/example/student/StudentDTO.java
@@ -1,14 +1,16 @@
 package com.capellax.example.student;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 
 public record StudentDTO(
-        @NotEmpty
+        @NotEmpty(message = "First name cannot be empty!")
         String firstName,
 
-        @NotEmpty
+        @NotEmpty(message = "Last name cannot be empty!")
         String lastName,
 
+        @Email(message = "Email must be a valid email!")
         String email,
 
         Integer schoolId
